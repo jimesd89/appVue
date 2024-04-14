b97e47cc6b2a6d3733f8d8041802666f2c2bbb75
Jimena Palmero <jimena.palmero@macena.com.ar>
2024-04-14 15:56:54 -0300

    proyecto en vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <TheHeader title="The world"></TheHeader>
+  <TheResources></TheResources>
+</template>
+
+<script>
+
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resource/TheResources.vue';
+
+export default {
+  components: {
+    TheHeader,
+    TheResources
+  },
+  
+};
+</script>
